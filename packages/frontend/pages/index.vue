7c761045729aa91f3fb08adfcc40e08ad3c355c6
Decibelter <Decibelter@users.noreply.github.com>
2020-10-13 14:03:21 +1100

    add index page (#30)
    Co-authored-by: Andrew Persic <andrew.persic@twobulls.com>

---
+++ packages/frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          Redirecting to Homeless Data Wallet...
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { Vue, Component } from 'nuxt-property-decorator'
+@Component
+export default class Index extends Vue {}
+</script>
